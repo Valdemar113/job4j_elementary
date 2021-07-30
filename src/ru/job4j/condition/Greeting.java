58843b16b47fca9999e58ca32963e902e805602e
Valdemar113 <daodando13@gmail.com>
2021-07-30 15:51:27 +0300

    9.1. String [#237407]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea ="I like Java!" ;
+        System.out.println(idea);
+        String id= idea+ "But I am a newbie";
+        int year= 2021;
+        String separate=" ";
+        String idy=id+separate+year;
+        System.out.println(idy);
+
+
+    }
+}
