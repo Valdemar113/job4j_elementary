875057d09f025b50f8d04ab5c4db5fe6db72c25d
Valdemar113 <daodando13@gmail.com>
2021-07-30 16:38:48 +0300

    9.2. Checkstyle [#4123]

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -2,14 +2,13 @@ package ru.job4j.condition;
 
 public class Greeting {
     public static void main(String[] args) {
-        String idea ="I like Java!" ;
+        String idea = "I like Java!";
         System.out.println(idea);
-        String id= idea+ "But I am a newbie";
-        int year= 2021;
-        String separate=" ";
-        String idy=id+separate+year;
+        String id = idea + "But I am a newbie";
+        int year = 2021;
+        String separate = " ";
+        String idy = id + separate + year;
         System.out.println(idy);
-
 
     }
 }
--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -2,17 +2,19 @@ package ru.job4j.converter;
 
 public class Converter {
     public static float rubleToEuro(float value) {
-        float rsl = value/70;
+        float rsl = value / 70;
         return rsl;
     }
+
     public static float rubleToDollar(float value) {
-        float rsl = value/60;
+        float rsl = value / 60;
         return rsl;
     }
+
     public static void main(String[] args) {
-        float euro=Converter.rubleToEuro(140);
+        float euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro");
-        float dollar=Converter.rubleToDollar(180);
-        System.out.println("180 rubles are " +dollar+ " dollar");
+        float dollar = Converter.rubleToDollar(180);
+        System.out.println("180 rubles are " + dollar + " dollar");
     }
 }
