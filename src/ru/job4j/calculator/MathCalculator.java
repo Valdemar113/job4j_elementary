4c19ba183f47a707dcb9a5041fca5fc9a3120fad
Valdemar113 <daodando13@gmail.com>
2021-08-01 17:30:02 +0300

    1.1. Что такое import [#504773 #183995]

--- src/ru/job4j/calculator/MathCalculator.java
+++ src/ru/job4j/calculator/MathCalculator.java
@@ -2,21 +2,14 @@ package ru.job4j.calculator;
 import static ru.job4j.math.MathFunction.*;
 
 public class MathCalculator {
-    public static double sumandmultiply(double first, double second) {
+    public static double sumAndMultiply(double first, double second) {
         return sum(first, second)
                 + multiply(first, second);
 
     }
 
-    public static void main(String[] args) {
-        System.out.println("Результат расчета равен: " + sumandmultiply(10, 20));
-        System.out.println("Результат расчета (сумма разности и деления двух чисел) равен : " + sumrazndiv(10, 5));
-        System.out.println("Результат расчета (сумма вычисления всех четырех операций) равен : " + sumsmrd(10, 20));
-
-    }
-
-    public static double sumrazndiv(double first, double second) {
-        return razn(first, second)
+    public static double diffAndDiv(double first, double second) {
+        return diff(first, second)
                 + div(first, second);
 
     }
@@ -24,7 +17,15 @@ public class MathCalculator {
     public static double sumsmrd(double first, double second) {
         return sum(first, second)
                 + multiply(first, second)
-                + razn(first, second)
+                + diff(first, second)
                 + div(first, second);
     }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+        System.out.println("Результат расчета (сумма разности и деления двух чисел) равен : " + diffAndDiv(10, 5));
+        System.out.println("Результат расчета (сумма вычисления всех четырех операций) равен : " + sumsmrd(10, 20));
+
+    }
+
 }
