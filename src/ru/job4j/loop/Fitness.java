4bd65a067ef56c4620e4c9a2b0bcb8849585193d
Valdemar113 <daodando13@gmail.com>
2021-08-12 18:39:48 +0300

    5.4. Протеиновая диета [#156313 #188803]

--- src/ru/job4j/loop/Fitness.java
+++ src/ru/job4j/loop/Fitness.java
@@ -2,15 +2,12 @@ package ru.job4j.loop;
 
 public class Fitness {
     public static int calc(int ivan, int nik) {
-        int month = 1;
-        while ((ivan *= 3) < (nik *= 2)) {
-           System.out.println(month);
+        int month = 0;
+        while (ivan <= nik) {
+            ivan *= 3;
+            nik *= 2;
            month += 1;
         }
         return month;
     }
-
-    /* public static void main(String[] args) {
-        System.out.println(calc(95, 90));
-    } */
 }
