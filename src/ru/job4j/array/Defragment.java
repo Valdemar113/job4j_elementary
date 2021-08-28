d933b320f87b1cd5c0b0b4abcb05406c1c390664
Valdemar113 <daodando13@gmail.com>
2021-08-28 18:24:19 +0300

    6.8. Дефрагментация массива [#225]

--- src/ru/job4j/array/Defragment.java
+++ src/ru/job4j/array/Defragment.java
@@ -10,7 +10,6 @@ public class Defragment {
                         array[index] = array[notNullIndex];
                         array[notNullIndex] = null;
                         break;
-
                     }
                 }
             }
